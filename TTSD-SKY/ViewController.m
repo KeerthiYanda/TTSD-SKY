cc402a7f72ee6f094f64aaa85063323d79af505e
KeerthiYanda <keethu.yanda@gmail.com>
2015-06-17 17:39:52 -0500

    Assessment 2

--- TTSD-SKY/ViewController.m
+++ TTSD-SKY/ViewController.m
@@ -7,10 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "AFHTTPRequestOperationManager.h"
+//#import "AudioFile.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#define BASE_URL "http://translate.google.com/translate_tts?tl=en&q="
+
 
 @interface ViewController ()
 
+@property (strong, nonatomic) AVAudioPlayer *audioPlayer;
 @end
+
 
 @implementation ViewController
 
@@ -23,5 +31,62 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)SpeakOn:(id)sender {
+    
+    [_sentenceTextField resignFirstResponder];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    NSString *sentense = [_sentenceTextField.text stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    
+    NSString *url = [NSString stringWithFormat:@"%s%@", BASE_URL, sentense];
+    
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    [manager GET:url parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        //        NSString *string = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        operation.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"audio/mpeg"];
+        //AudioPlayer *player = (AudioPlayer *) responseObject;
+        //NSSound *round = [NSSound soundNamed:@"replace-with-file-name-without-the-extension"];
+        //NSString *responseString=[NSString stringWithUTF8String:[responseObject bytes]];
+        
+        //
+        NSLog(@"NSObject: %@", responseObject);
+        
+        NSData *audioData = responseObject;
+        
+        [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+        [[AVAudioSession sharedInstance] setActive:YES error:nil];
+        
+        self.audioPlayer = [[AVAudioPlayer alloc] initWithData:audioData error:nil]; // audioPlayer must be a strong property. Do not create it locally
+        
+        [self.audioPlayer prepareToPlay];
+        [self.audioPlayer play];
+        
+        // NSLog(@"responseString: %@", responseString);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                        message:[error description]
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Ok"
+                                              otherButtonTitles:nil, nil];
+        [alert show];
+    }];
+    
+    
+    
+}
+
+
+//NSURL *URL = [NSURL URLWithString:url];
+
+//NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+
+//AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+
+
 
 @end
