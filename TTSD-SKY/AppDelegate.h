88b2cf5ac4845ad31000f1d1488ae3b49499553f
Yanda <kyd36@kc-sce463lab-12.kc.umkc.edu>
2015-06-17 12:49:49 -0500

    Initial Commit

---
+++ TTSD-SKY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTSD-SKY
+//
+//  Created by Yanda, Keerthi (UMKC-Student) on 6/17/15.
+//  Copyright (c) 2015 Yanda, Keerthi (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTSD-SKYTests/TTSD_SKYTests.m
@@ -0,0 +1,40 @@
+//
+//  TTSD_SKYTests.m
+//  TTSD-SKYTests
+//
+//  Created by Yanda, Keerthi (UMKC-Student) on 6/17/15.
+//  Copyright (c) 2015 Yanda, Keerthi (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TTSD_SKYTests : XCTestCase
+
+@end
+
+@implementation TTSD_SKYTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
